2b2224383ff6582520eef10b44aa607f3a41d7cb
ayserNii <147005714+Resyourbae@users.noreply.github.com>
2024-10-27 19:57:33 +0700

    Add files via upload

---
+++ koneksi.php
@@ -0,0 +1,10 @@
+<?php 
+
+    $server = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "db_tokoonline";
+
+    $con = mysqli_connect("localhost","root","","db_tokoonline");
+
+?>
